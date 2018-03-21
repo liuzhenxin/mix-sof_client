938e5c0517ad060da156ffb5c7d325112427ddf0
stoneskill <244540499@qq.com>
2018-03-21 15:52:51 +0800

    test all ok for test

--- src/sof_client/x_test.cpp
+++ src/sof_client/x_test.cpp
@@ -18,14 +18,14 @@ int main(int argc, char * argv[])
 	ULONG ulUserListLen = 1024;
 
 	unsigned char plain[32] = { 0 };
-	unsigned char signature[2048] = { 0 };
-	unsigned char sign_cert[2048] = { 0 };
-	unsigned char crypt_cert[2048] = { 0 };
+	unsigned char * signature = new unsigned char[1024 * 8];
+	unsigned char * sign_cert = new unsigned char[1024 * 8];
+	unsigned char * crypt_cert = new unsigned char[1024 * 8];
 
 	ULONG plain_len = sizeof(plain);
-	ULONG sign_cert_len = sizeof(sign_cert);
-	ULONG crypt_cert_len = sizeof(crypt_cert);
-	ULONG signature_len = sizeof(signature);
+	ULONG sign_cert_len = 1024 * 8;
+	ULONG crypt_cert_len = 1024 * 8;
+	ULONG signature_len = 1024 * 8;
 	int i = 0;
 
 	unsigned char info[2048] = { 0 };
@@ -35,6 +35,17 @@ int main(int argc, char * argv[])
 	CK_SKF_FUNCTION_LIST *ckpFunctions = NULL;
 
 	ULONG ulResult = 0;
+
+	const char * xmlData = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" \
+		"<!-- \n" \
+		"XML Security Library example: Original XML doc file for sign3 example. \n" \
+		"-->\n" \
+		"<Envelope xmlns=\"urn:envelope\">\n" \
+		"  <Data>\n" \
+		"	Hello,ABCDEFG World!\n" \
+		"  </Data>\n" \
+		"</Envelope>\n";
+
 
 	ulResult = SOF_InitializeLibraryNative("WTSKFInterface.dll", &ckpFunctions);
 	if (ulResult)
@@ -48,72 +59,94 @@ int main(int argc, char * argv[])
 		goto end;
 	}
 
-	ulResult = SOF_Login(ckpFunctions, "RT_SM_CON", "88888888");
+	ulResult = SOF_Login(ckpFunctions, "11-rsa", "88888888");
 	if (ulResult)
 	{
 		goto end;
 	}
 
-	ulResult = SOF_ExportUserCert(ckpFunctions, "RT_SM_CON", sign_cert, &sign_cert_len);
+	ulResult = SOF_ExportUserCert(ckpFunctions, "11-rsa", sign_cert, &sign_cert_len);
 	if (ulResult)
 	{
 		goto end;
 	}
 
-	ulResult = SOF_ExportExChangeUserCert(ckpFunctions, "RT_SM_CON", crypt_cert, &crypt_cert_len);
+	ulResult = SOF_ExportExChangeUserCert(ckpFunctions, "11-rsa", crypt_cert, &crypt_cert_len);
 	if (ulResult)
 	{
 		goto end;
 	}
 
-	ulResult = SOF_SetSignMethod(ckpFunctions, SGD_SM3_SM2);
+	ulResult = SOF_SetSignMethod(ckpFunctions, SGD_SM3_RSA);
 	if (ulResult)
 	{
 		goto end;
 	}
 
-
-
+	ulResult = SOF_SignDataXML(ckpFunctions, "11-rsa", (BYTE*)xmlData, strlen(xmlData), signature, &signature_len);
+	if (ulResult)
 	{
+		goto end;
+	}
 
+	ulResult = SOF_VerifySignedDataXML(ckpFunctions, signature, signature_len);
+	if (ulResult)
+	{
+		goto end;
+	}
 
-		const char * xmlData = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" \
-			"<!-- \n" \
-			"XML Security Library example: Original XML doc file for sign3 example. \n" \
-			"-->\n" \
-			"<Envelope xmlns=\"urn:envelope\">\n" \
-			"  <Data>\n" \
-			"	Hello,ABCDEFG World!\n" \
-			"  </Data>\n" \
-			"</Envelope>\n";
-
-		ulResult = SOF_SignDataXML(ckpFunctions, "RT_SM_CON", (BYTE*)xmlData, strlen(xmlData), signature, &signature_len);
+	for (i = 1; i < 7; i++)
+	{
+		info_len = sizeof(info);
+		memset(info, 0, info_len);
+		ulResult = SOF_GetXMLSignatureInfo(ckpFunctions, i, signature, signature_len, info, &info_len);
 		if (ulResult)
 		{
 			goto end;
 		}
+		printf("%s\n", info);
+	}
+	
 
-		ulResult = SOF_VerifySignedDataXML(ckpFunctions, signature, signature_len);
+	signature_len = 1024 * 8;
+	ulResult = SOF_SignMessage(ckpFunctions, "11-rsa", 0, plain, plain_len, signature, &signature_len);
+	if (ulResult)
+	{
+		goto end;
+	}
+
+	ulResult = SOF_VerifySignedMessage(ckpFunctions, signature, signature_len, NULL, 0);
+	if (ulResult)
+	{
+		goto end;
+	}
+
+	for (i = 1; i < 4; i++)
+	{
+		info_len = sizeof(info);
+		ulResult = SOF_GetInfoFromSignedMessage(ckpFunctions, i, signature, signature_len, info, &info_len);
 		if (ulResult)
 		{
 			goto end;
 		}
+	}
+	
 
 
-		for (i = 1; i < 7; i++)
-		{
-			info_len = sizeof(info);
-			memset(info, 0, info_len);
-			ulResult = SOF_GetXMLSignatureInfo(ckpFunctions, i, signature, signature_len, info, &info_len);
-			if (ulResult)
-			{
-				goto end;
-			}
-			printf("%s\n", info);
-		}
+	signature_len = 1024 * 8;
+	ulResult = SOF_SignMessage(ckpFunctions, "11-rsa", 1, plain, plain_len, signature, &signature_len);
+	if (ulResult)
+	{
+		goto end;
 	}
 
-	//ulResult = SOF_SignData(ckpFunctions, "RT_SM_CON", plain, plain_len, signature, &signature_len);
+	ulResult = SOF_VerifySignedMessage(ckpFunctions, signature, signature_len, plain, plain_len);
+	if (ulResult)
+	{
+		goto end;
+	}
+
+	//ulResult = SOF_SignData(ckpFunctions, "11-rsa", plain, plain_len, signature, &signature_len);
 	//if (ulResult)
 	//{
 	//	goto end;
@@ -124,46 +157,11 @@ int main(int argc, char * argv[])
 	//{
 	//	goto end;
 	//}
-	//signature_len = sizeof(signature);
-	//ulResult = SOF_SignMessage(ckpFunctions, "RT_SM_CON",0, plain, plain_len, signature, &signature_len);
-	//if (ulResult)
-	//{
-	//	goto end;
-	//}
 
-	//ulResult = SOF_VerifySignedMessage(ckpFunctions,signature, signature_len, NULL, 0);
-	//if (ulResult)
-	//{
-	//	goto end;
-	//}
+	
 
-	//for (i = 1; i < 4; i++)
-	//{
-	//	info_len = sizeof(info);
-	//	ulResult = SOF_GetInfoFromSignedMessage(ckpFunctions, i, signature, signature_len, info, &info_len);
-	//	if (ulResult)
-	//	{
-	//		goto end;
-	//	}
-	//}
-	//
-
-
-	//signature_len = sizeof(signature);
-	//ulResult = SOF_SignMessage(ckpFunctions, "RT_SM_CON", 1, plain, plain_len, signature, &signature_len);
-	//if (ulResult)
-	//{
-	//	goto end;
-	//}
-
-	//ulResult = SOF_VerifySignedMessage(ckpFunctions, signature, signature_len, plain, plain_len);
-	//if (ulResult)
-	//{
-	//	goto end;
-	//}
-
-	//signature_len = sizeof(signature);
-	//ulResult = SOF_SignFile(ckpFunctions, "RT_SM_CON", "D:/test.txt", signature, &signature_len);
+	//signature_len = 1024 * 8;
+	//ulResult = SOF_SignFile(ckpFunctions, "11-rsa", "D:/test.txt", signature, &signature_len);
 	//if (ulResult)
 	//{
 	//	goto end;
@@ -189,7 +187,7 @@ int main(int argc, char * argv[])
 	}
 
 
-	ulResult = SOF_DecryptFile(ckpFunctions, "RT_SM_CON", "D:/cipher.txt", "D:/plain.txt" );
+	ulResult = SOF_DecryptFile(ckpFunctions, "11-rsa", "D:/cipher.txt", "D:/plain.txt" );
 	if (ulResult)
 	{
 		goto end;
